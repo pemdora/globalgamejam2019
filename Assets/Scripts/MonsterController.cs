5d8179ee0c13dadfba27a5b390d05e290d77920b
hubertmarret <hubert.marret@gmail.com>
2019-01-25 22:44:16 +0100

    Adding HubScene and a monster displacement

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour {
+
+    void Start()
+    {
+        MoveTo(GameObject.Find("Kitchen"));
+    }
+
+    // Move to a position
+    public void MoveTo(Vector3 target)
+    {
+        GetComponent<NavMeshAgent>().SetDestination(target);
+    }
+
+    // Move to a gameobject
+    public void MoveTo(GameObject target)
+    {
+        GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+    }
+
+	void Update () {
+
+        // moving the monster by click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
+    }
+}
