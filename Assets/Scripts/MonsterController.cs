b0d470d8ed605fa8ab9286961102972c748d7ec9
tarazhong <zhong.tara@gmail.com>
2019-01-25 23:14:28 +0100

    Fixed movment for character

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonsterController : MonoBehaviour {
-    
+public class MonsterController : MonoBehaviour
+{
+    public bool changedTarget;
+    public Vector3 target;
+
+    private void Start()
+    {
+        MovementManager.instance.AssignPOI(this);
+        changedTarget = false;
+    }
 
     // Move to a position
     public void MoveTo(Vector3 target)
@@ -18,8 +26,23 @@ public class MonsterController : MonoBehaviour {
         GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
     }
 
-	void Update () {
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, target) > 1.5f)
+        {
+            MoveTo(target);
+        }
+        else
+        {
+            if (!changedTarget)
+            {
+                Debug.Log("changedTarget");
+                changedTarget = true;
+                MovementManager.instance.AssignPOI(this);
+            }
+        }
 
+        /*
         // moving the monster by click
         if (Input.GetMouseButtonDown(0))
         {
@@ -30,6 +53,6 @@ public class MonsterController : MonoBehaviour {
             {
                 GetComponent<NavMeshAgent>().SetDestination(hit.point);
             }
-        }
+        }*/
     }
 }
---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour {
+
+    public List<GameObject> pointOfInterest;
+    private List<GameObject> freePointOfInterest;
+
+    public static MovementManager instance;
+    
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+        {
+            //if not, set instance to this
+            instance = this;
+        }
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AssignPOI(MonsterController monsterC)
+    {
+        int index = Random.Range(0, pointOfInterest.Count-1);
+        Debug.Log("index " + index);
+
+        monsterC.target =  pointOfInterest[index].transform.position;
+        monsterC.changedTarget = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
