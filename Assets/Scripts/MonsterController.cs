c6243bee75ceec997df9220cd0b2b24d2724a762
tarazhong <zhong.tara@gmail.com>
2019-01-26 14:09:34 +0100

    Finished reaction to object

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class MonsterController : MonoBehaviour
 {
@@ -12,12 +13,18 @@ public class MonsterController : MonoBehaviour
     [HideInInspector]
     public float timeLeft;
 
+    [SerializeField]
+    private Image emoteImg;
+
+    private bool playingImgAnimation;
+
     public List<ObjectData> reactionObjects;
 
     private void Start()
     {
         MovementManager.instance.AssignPOI(this);
         doingAction = true;
+        playingImgAnimation = false;
     }
 
     // Move to a position
@@ -65,17 +72,59 @@ public class MonsterController : MonoBehaviour
     // Move to a gameobject
     public void ReactToObject(GameObject _obj)
     {
-        foreach (ObjectData objData in reactionObjects)
+        if (!playingImgAnimation)
         {
-            switch (objData.reaction)
+            foreach (ObjectData objData in reactionObjects)
             {
-                case ObjectData.REACTION.Love:
-                    Debug.Log("I Loove that");
-                    break;
-                case ObjectData.REACTION.Hate:
-                    Debug.Log("I hate that");
-                    break;
+                if (_obj.name.Equals(objData.obj.name))
+                {
+                    switch (objData.reaction)
+                    {
+                        case ObjectData.REACTION.Love:
+                            Debug.Log("Love");
+                            emoteImg.sprite = objData.emote;
+                            StartCoroutine(FadeINandOutImage(0.7f, false, emoteImg, 0.5f));
+                            break;
+                        case ObjectData.REACTION.Hate:
+                            Debug.Log("Hate");
+                            emoteImg.sprite = objData.emote;
+                            StartCoroutine(FadeINandOutImage(0.7f, false, emoteImg, 0.5f));
+                            break;
+                    }
+                }
             }
         }
     }
+
+    IEnumerator FadeINandOutImage(float alpha,bool fadeAway, Image img, float speed)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = alpha; i >= 0; i -= Time.deltaTime * speed)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+            img.enabled = false;
+            playingImgAnimation = false;
+        }
+        // fade from transparent to opaque
+        else
+        {
+            playingImgAnimation = true;
+            img.enabled = true;
+            // loop over 0.5 second
+            for (float i = 0; i <= alpha; i += Time.deltaTime * speed)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+            StartCoroutine(FadeINandOutImage(alpha, true, img, speed));
+        }
+    }
+
 }
