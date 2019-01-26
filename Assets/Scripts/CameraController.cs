4df48197ee1007f06c5f21b376a0a8f0dd806a96
hubertmarret <hubert.marret@gmail.com>
2019-01-26 23:45:58 +0100

    Upgrade Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public float panSpeed = 5.0f;
-    public float boundery = 30.0f;
+    public float XMin = 30.0f;
+    public float XMax = 30.0f;
+    public float YMin = 30.0f;
+    public float YMax = 30.0f;
 
-	void Update () {
+    void Update () {
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
         {
             Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize - 1, 6);
         }
-        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
         {
             Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize + 1, 14);
         }
-        if (Input.GetMouseButton(2))
+        if (Camera.main.orthographicSize < 13 && Input.GetMouseButton(1))
         {
-            float horizontal = Input.GetAxis("Mouse X");
-            float vertical = Input.GetAxis("Mouse Y");
+            float horizontal = Input.GetAxis("Mouse X") * panSpeed;
+            float vertical = Input.GetAxis("Mouse Y") * panSpeed;
 
-            Vector3 newPos = new Vector3(Camera.main.transform.position.x - horizontal * panSpeed, Camera.main.transform.position.y - vertical * panSpeed);
-            
-            if(newPos.x < boundery && newPos.x > -boundery && newPos.y < boundery && newPos.y > -boundery)
+            Vector3 newPos = new Vector3(Camera.main.transform.position.x + horizontal, Camera.main.transform.position.y + vertical, Camera.main.transform.position.z - horizontal);
+            //newPos = new Vector3(newPos.x - vertical, newPos.y, newPos.z + vertical);
+
+            if (newPos.x < XMax && newPos.x > -XMin && newPos.y < YMax && newPos.y > -YMin)
             {
                 Camera.main.transform.position = newPos;
             }
