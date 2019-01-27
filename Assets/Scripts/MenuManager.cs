8bee24993b8380cfed794f2b820338a3e9afaefe
tarazhong <zhong.tara@gmail.com>
2019-01-27 03:22:33 +0100

    Reapplied main menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -34,4 +34,32 @@ public class MenuManager : MonoBehaviour {
     {
         Application.Quit();
     }
+
+
+    public void PauseGame()
+    {
+        Invoke("PauseTheGame", 0.2f);
+    }
+
+    private void PauseTheGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void SwapState()
+    {
+        if (Time.timeScale > 0)
+        {
+            PauseGame();
+        }
+        else
+        {
+            UnpauseGame();
+        }
+    }
 }
